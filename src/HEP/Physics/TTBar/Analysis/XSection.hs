f00ceca454e0f935a571c54a8dc3aa409178ff9c
ianwookim <ianwookim@gmail.com>
2011-03-09 14:26:57 +0000

    refactor SM model xsection to SM.hs

--- src/HEP/Physics/TTBar/Analysis/XSection.hs
+++ src/HEP/Physics/TTBar/Analysis/XSection.hs
@@ -17,9 +17,6 @@ import HEP.Physics.TTBar.Model.Exotic
 
 data MachineType = PP | PPbar
 
--- | top quark pole mass 
-mt :: Double 
-mt = 174.3
 
 data ColorExoticArg = SA { 
   sa_c0 :: Double,
@@ -72,32 +69,6 @@ totalXSec_qq_exotic (SA c0 c2 y mt mphi) pcalc (alphaS, s, costh) =
 
 
 
--- | Parton-level total cross section of SM : qqbar
---   reference : Nason, Dawson, Ellis <http://141.211.99.67:8080/pub/Study/TTbarPhysics/NasonDawsonEllis1.pdf>
-
-totalXSec_qq_SM :: Double -> Double -> Double
-totalXSec_qq_SM alphaS s =
-  if s > 4.0*mt^(2::Int) 
-     then let rho = 4.0 * mt^(2::Int) / s
-              beta = sqrt ( 1.0 - rho ) 
-              fqqbar = (pi*beta*rho) / 27.0 * (2.0+rho) 
-          in  alphaS^(2::Int) / mt^(2::Int) * fqqbar
-     else 0 
-
--- | Parton-level total cross section of SM : gluglu
---   reference : Nason, Dawson, Ellis <http://141.211.99.67:8080/pub/Study/TTbarPhysics/NasonDawsonEllis1.pdf>
-
-totalXSec_gg_SM :: Double -> Double -> Double
-totalXSec_gg_SM alphaS s =
-  if s > 4.0*mt^(2::Int) 
-     then let rho = 4.0 * mt^(2::Int) / s
-              beta = sqrt ( 1.0 - rho ) 
-              fgg = (pi*beta*rho) / 192.0 
-                    * ( 1.0 / beta * (rho^(2::Int) + 16.0*rho+16.0)  
-                        * log ((1.0+beta)/(1.0-beta)) 
-                        - 28.0 - 31.0 * rho )  
-          in  alphaS^(2::Int) / mt^(2::Int) * fgg
-     else 0 
 
 
 partonXsecIntegrand :: MachineType 
---
+++ src/HEP/Physics/TTBar/Model/SM.hs
@@ -0,0 +1,32 @@
+module HEP.Physics.TTBar.Model.SM where
+
+-- | top quark pole mass 
+mt :: Double 
+mt = 174.3
+
+-- | Parton-level total cross section of SM : qqbar
+--   reference : Nason, Dawson, Ellis <http://141.211.99.67:8080/pub/Study/TTbarPhysics/NasonDawsonEllis1.pdf>
+
+totalXSec_qq_SM :: Double -> Double -> Double
+totalXSec_qq_SM alphaS s =
+  if s > 4.0*mt^(2::Int) 
+     then let rho = 4.0 * mt^(2::Int) / s
+              beta = sqrt ( 1.0 - rho ) 
+              fqqbar = (pi*beta*rho) / 27.0 * (2.0+rho) 
+          in  alphaS^(2::Int) / mt^(2::Int) * fqqbar
+     else 0 
+
+-- | Parton-level total cross section of SM : gluglu
+--   reference : Nason, Dawson, Ellis <http://141.211.99.67:8080/pub/Study/TTbarPhysics/NasonDawsonEllis1.pdf>
+
+totalXSec_gg_SM :: Double -> Double -> Double
+totalXSec_gg_SM alphaS s =
+  if s > 4.0*mt^(2::Int) 
+     then let rho = 4.0 * mt^(2::Int) / s
+              beta = sqrt ( 1.0 - rho ) 
+              fgg = (pi*beta*rho) / 192.0 
+                    * ( 1.0 / beta * (rho^(2::Int) + 16.0*rho+16.0)  
+                        * log ((1.0+beta)/(1.0-beta)) 
+                        - 28.0 - 31.0 * rho )  
+          in  alphaS^(2::Int) / mt^(2::Int) * fgg
+     else 0 
