c60dbc4e45dbee644dc31e2251d06695f163468f
ianwookim <ianwookim@gmail.com>
2012-01-18 23:21:19 +0000

    correct implementation of counting using processOneEvent.

--- lib/HEP/Physics/TTBar/Analysis/TopPairParton.hs
+++ lib/HEP/Physics/TTBar/Analysis/TopPairParton.hs
@@ -2,6 +2,7 @@ module HEP.Physics.TTBar.Analysis.TopPairParton where
 
 import HEP.Util.Functions
 import HEP.Parser.LHEParser.Type
+import Control.Monad
 import Control.Monad.Trans
 import Data.Enumerator
 import qualified Data.Enumerator.List as EL
@@ -33,25 +34,35 @@ identifyTopPair lst =
        (Just tmom, Just atmom) -> Just (TopPair tmom atmom)
        _ -> Nothing 
 
-checkTTBarEvent :: Maybe (Maybe (a,b,[DecayTop PtlIDInfo])) -> Maybe TopPair
-checkTTBarEvent mel = do
-  do mdetail <- mel 
-     (_,_,dtops) <- mdetail
+checkTTBarEvent :: Maybe (a,b,[DecayTop PtlIDInfo]) -> Maybe TopPair
+checkTTBarEvent el = do
+  do (_,_,dtops) <- el
      identifyTopPair dtops
+
+
+proceedOneEvent :: (Monad m) => (a -> Iteratee a m ()) -> Iteratee a m ()
+proceedOneEvent action = EL.head >>= maybe (return ()) (\x -> action x >> proceedOneEvent action)
 
 
 proceedWithActionForTopPair :: (Monad m) => 
                                (TopPair -> Maybe (Iteratee (Maybe (a,b,[DecayTop PtlIDInfo])) m ())) 
                             -> Iteratee (Maybe (a,b,[DecayTop PtlIDInfo])) m () 
-proceedWithActionForTopPair action = do 
-  mel <- EL.head
-  maybe (return ()) (\x -> x >> proceedWithActionForTopPair action)
-        $ checkTTBarEvent mel >>= action 
+proceedWithActionForTopPair action = proceedOneEvent $ maybe (return ()) id 
+                                                       . (action <=< checkTTBarEvent) 
+--  proceedOneEvent $ (\x -> x >> proceedWithActionForTopPair action)
+--  
 
 
-
+{-
 showNonTTBarEvent :: (MonadIO m) => DecayTopIteratee a b m ()
-showNonTTBarEvent = do 
+showNonTTBarEvent = 
+    proceedOneEvent $ \el -> do (_,_,dtops) <- el 
+                                case identifyTopPair dtops of 
+                                  Just _ -> return () 
+                                  Nothing -> return () --  mapM_ (liftIO . print . fmap pdgid ) dtops 
+-}
+
+{- do 
   mel <- EL.head
   maybe (return ()) (\x -> x >> showNonTTBarEvent)
         $ do mdetail <- mel 
@@ -59,7 +70,7 @@ showNonTTBarEvent = do
              case identifyTopPair dtops of 
                Just _ -> return (return () )
                Nothing -> return (mapM_ (liftIO . print . fmap pdgid ) dtops )
-
+-}
 
 
 showTTBarEvent :: (MonadIO m) => Iteratee (Maybe (a,b,[DecayTop PtlIDInfo])) m () 
